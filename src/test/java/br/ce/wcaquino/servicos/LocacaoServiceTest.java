27e15636ee1844590387baf4b2521ad9fbe1baaf
Maurício Eberlin Pereira <mauricio.eberlin@hotmail.com>
2019-10-31 14:35:42 -0300

    [04] - Formas de dividir um teste

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -1,12 +1,12 @@
 package br.ce.wcaquino.servicos;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.junit.Assert.assertThat;
 
 import java.util.Date;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -15,27 +15,24 @@ import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
 	
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+	
 	@Test
-	public void teste() {
+	public void testeLocacao() {
 		//cenario
 		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Usuario 1");
 		Filme filme = new Filme("Filme 1", 2, 5.0);
-		
-		//String variavelNoMesmoPacoteOnly = service.vDefault;
+	  //String variavelNoMesmoPacoteOnly = service.vDefault;
 		
 		//acao
 		Locacao locacao = service.alugarFilme(usuario, filme);
 		
-		//verificacao (ficar atento na ordem (ESPERADO, RECEBIDO, DELTA))
-		//assertEquals(5.0, locacao.getValor(), 0.01);
-		//assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		//assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-		
-		assertThat(locacao.getValor(), is(equalTo(5.0)));
-		assertThat(locacao.getValor(), is(not(6.0)));
-		assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
-		assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+		//verificacao
+		error.checkThat(locacao.getValor(), is(equalTo(5.0)));
+		error.checkThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		error.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
 	}
 	
 }
